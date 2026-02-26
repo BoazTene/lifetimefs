a59564d66dd1eb93568999da21f8e0ce0302d91f
Boaz Tene <boaztene2005@gmail.com>
2026-02-26 13:53:34 +0200

    Creates basic cli parser

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,32 @@
+use clap::{CommandFactory, Parser, Subcommand};
+use clap_help::Printer;
+use std::path::PathBuf;
+
+
+#[derive(Parser)]
+#[command[author="boaztene", about="A time-traveling filesystem written in Rust."]]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Mount {
+        mount_point: PathBuf,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Cli::parse();
+
+    match &args.command {
+        Some(Commands::Mount { mount_point }) => {
+            println!("mounting at: {:?}", mount_point);
+        }
+
+        None => {
+            Printer::new(Cli::command()).print_help();
+        }
+    }
 }
